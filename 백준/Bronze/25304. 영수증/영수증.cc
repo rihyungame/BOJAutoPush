8dce451434e94e81fcb8f5f05f45f268ecb67011
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-03-19 17:43:24 +0900

    [Bronze V] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+	int totalPrice, nTotal, temp = 0;
+
+	std::cin >> totalPrice >> nTotal;
+
+	int* stuffPrice = new int(nTotal);
+	int* nStuff = new int(nTotal);
+
+	for (int i = 0; i < nTotal; i++) {
+		std::cin >> stuffPrice[i] >> nStuff[i];
+		temp += stuffPrice[i] * nStuff[i];
+	}
+
+	if (temp == totalPrice)
+		std::cout << "Yes";
+	else {
+		std::cout << "No";
+	}
+    return 0;
+
+}

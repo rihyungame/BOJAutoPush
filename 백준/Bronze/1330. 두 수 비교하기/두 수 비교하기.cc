d7bd3c42382caeba52826eac168a9611dbebb51c
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-03-19 11:39:36 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	int a, b;
+	std::cin >> a >> b;
+	if (a < b)
+		std::cout << "<";
+	else if (a > b)
+		std::cout << ">";
+	else
+		std::cout << "==";
+	std::cout << std::endl;
+
+	return 0;
+}

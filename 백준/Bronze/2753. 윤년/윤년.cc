9c47cb442d68105bf8a7a5832f87799a308c5278
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-03-19 12:23:45 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	int year;
+	std::cin >> year;
+	if (year % 4 == 0) {
+		if (year % 100 == 0 && year % 400 != 0)
+			std::cout << "0";
+		else
+			std::cout << "1";
+	}
+	else
+		std::cout << "0";
+
+	return 0;
+}

0f0d21adc3a93d7c5ec43979fb0871f3770eedf6
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-03-19 19:55:49 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+	int n;
+	std::cin.tie(NULL);
+	std::cout.sync_with_stdio(false);
+
+	std::cin >> n;
+	int* temp1 = new int[n];
+	int* temp2 = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> temp1[i] >> temp2[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		std::cout << "Case #" << i+1 << ": " << temp1[i] + temp2[i] << "\n";
+	}
+
+	return 0;
+}

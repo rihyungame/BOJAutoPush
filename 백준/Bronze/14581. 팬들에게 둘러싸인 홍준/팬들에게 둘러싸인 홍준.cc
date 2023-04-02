9a29ae142e3245d5003ab0e7f6246803599761c0
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-04-02 17:40:19 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string s;
+	std::cin >> s;
+	std::cout << ":fan:";
+	std::cout << ":fan:";
+	std::cout << ":fan:" << '\n';
+
+
+	std::cout << ":fan::";
+	std::cout << s;
+	std::cout << "::fan:" << '\n';
+
+	std::cout << ":fan:";
+	std::cout << ":fan:";
+	std::cout << ":fan:" << '\n';
+	return 0;
+}

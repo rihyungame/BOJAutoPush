6b8896d76c634d41fac4b8665c51d2e6c582f23c
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-04-08 07:52:19 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{	
+	int a, b; 
+	std::cin >> a >> b;
+	if (a > b)
+		std::cout << "1";
+	else if (a < b)
+		std::cout << "-1";
+	else
+		std::cout << "0";
+	return 0;
+}

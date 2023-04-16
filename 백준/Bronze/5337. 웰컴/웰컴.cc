b4641fb49050fc7a8cf61bbd4529f147b7953e49
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-04-17 01:58:24 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << ".  .   ." << "\n";
+    std::cout << "|  | _ | _. _ ._ _  _" << "\n";
+    std::cout << "|/\\|(/.|(_.(_)[ | )(/.";
+    return 0;
+}

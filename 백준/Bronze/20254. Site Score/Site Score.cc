26cb4ac0163a879db421b02f039c23c4f007458d
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-04-19 04:25:48 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int num1, num2, num3, num4;
+    std::cin >> num1 >> num2 >> num3 >> num4;
+    std::cout << 56 * num1 + 24 * num2 + 14 * num3 + 6 * num4;
+    return 0;
+}

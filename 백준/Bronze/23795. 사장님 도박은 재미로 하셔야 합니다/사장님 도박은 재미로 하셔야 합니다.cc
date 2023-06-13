220147b679fb56ec0df7aa9a68dab8f79b29c6f9
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-06-13 20:25:29 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.cc
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.cc
@@ -1,14 +1,15 @@
 #include <iostream>
 
 int main() {
-    long long int total = 0;
-    int n = 0;
-    while (true) {
-        std::cin >> n;
-        if (n == -1)
-            break;
-        total += n;
-    }
-    std::cout << total;
-    return 0;
+	long int total = 0;
+
+	while (true) {
+		long int n;
+		std::cin >> n;
+		if (n == -1)
+			break;
+		total += n;
+	}
+	std::cout << total;
+	return 0;
 }

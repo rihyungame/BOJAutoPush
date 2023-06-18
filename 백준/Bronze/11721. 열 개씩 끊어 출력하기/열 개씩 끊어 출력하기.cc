7a231dbaaec7e8618d7868b3b6afacb8a0101866
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-06-18 22:06:38 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string s;
+	std::cin >> s;
+	int multiple = 1;
+	int count = 0;
+	bool break_on = false;
+
+	while (true) {
+		for (int i = 0; i < 10; i++) {
+			if (count == s.length()) {
+				break_on = true;
+				break;
+			}
+			std::cout << s[i + multiple - 1];
+			count++;
+		}
+		if (break_on)
+			break;
+		std::cout << "\n";
+		multiple += 10;
+	}
+	return 0;
+}

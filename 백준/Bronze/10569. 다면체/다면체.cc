b94d54a7ba80bc72d1425dffecbc12d0c6d6ace2
rihyungame <59619810+rihyungame@users.noreply.github.com>
2023-07-08 11:01:21 +0900

    [Bronze III] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int n;
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int v, e;
+		std::cin >> v >> e;
+		std::cout << 2 - v + e << "\n";
+	}
+	return 0;
+}

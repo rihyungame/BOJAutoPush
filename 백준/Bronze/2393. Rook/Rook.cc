00ef01e2bb9e0c55fea781ec728c473ed0671d68
rihyungame <59619810+rihyungame@users.noreply.github.com>
2024-05-12 23:20:48 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+
+    std::cout << "  ___  ___  ___\n"
+        "  | |__| |__| |\n"
+        "  |           |\n"
+        "   \\_________/\n"
+        "    \\_______/\n"
+        "     |     |\n"
+        "     |     |\n"
+        "     |     |\n"
+        "     |     |\n"
+        "     |_____|\n"
+        "  __/       \\__\n"
+        " /             \\\n"
+        "/_______________\\\n";
+        
+	return 0;
+}

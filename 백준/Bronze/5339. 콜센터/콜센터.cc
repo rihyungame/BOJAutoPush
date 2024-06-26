a43559172505deaee776852c900f41a6e6dda2d1
rihyungame <59619810+rihyungame@users.noreply.github.com>
2024-06-26 23:15:29 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+
+	std::cout << R"(     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |)"; 
+	return 0;
+}

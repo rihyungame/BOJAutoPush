434d24428b5117c8586a0c9fde4697837eb8596d
rihyungame <59619810+rihyungame@users.noreply.github.com>
2024-10-10 19:17:38 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+    int n;
+    std::cin >> n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "LoveisKoreaUniversity";
+        std::cout << " ";
+    }
+    
+    return 0;
+}
